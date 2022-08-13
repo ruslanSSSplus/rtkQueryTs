f0a17126cec1e33c288cb318009c6a6dae3209d9
ruslanSSSplus <ruslanlyanov1999@gmail.com>
2022-08-13 19:40:31 +0300

    first commit

---
+++ src/services/PostService.ts
@@ -0,0 +1,14 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react";
+
+
+export const userAPI = createApi({
+    reducerPath: 'userAPI',
+    baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com/'}),
+    endpoints: (build) => ({
+        fetchAllUsers: build.query({
+            query: () => ({
+                url: '/users'
+            })
+        })
+    })
+})
